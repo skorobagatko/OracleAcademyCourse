4b7905d36ab38f2fbba826f94210a24ebbce734a
Stanislav Skorobahatko <skorobagatko@mail.com>
2018-03-25 14:35:59 +0300

    Refactored Human class

--- src/main/java/com/skorobahatko/lectures/lecture4/Human.java
+++ src/main/java/com/skorobahatko/lectures/lecture4/Human.java
@@ -5,16 +5,13 @@ public class Human<T> {
     private String name;
     private T instance;
     private Class<T> clazz;
-    private T[] arr;
 
     public Human(String name, Class<T> clazz) {
         this.name = name;
         this.clazz = clazz;
         try {
             instance = (T) clazz.newInstance();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
+        } catch (InstantiationException | IllegalAccessException e) {
             e.printStackTrace();
         }
     }
